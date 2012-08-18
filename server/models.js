b81877c74fcbdf82dfcab90d63feaa1e72af0218
laander <la@laander.com>
2012-08-17 18:18:30 -0700

    major restructure in backend exports architecture and frontend restructure

--- server/models.js
+++ server/models.js
@@ -1,39 +1,43 @@
 /**
  * Sequelize ORM Models
  */
-module.exports = function() {
-  var Sequelize = require('sequelize');
+module.exports = {
 
-  var db = new Sequelize('socialmoneyjs', 'root', 'root', {
-      host: "localhost",
-      port: 3306
-  });
+  init: function(){
 
-  var models = {
-     'User': db.define('User', {
-      id: Sequelize.INTEGER,
-      username: Sequelize.STRING,
-      password: Sequelize.STRING,
-      email: Sequelize.STRING
-    }),
-     
-    'Transaction': db.define('Transaction', {
-      id: Sequelize.INTEGER,
-      title: Sequelize.STRING,
-      description: Sequelize.TEXT,
-      date: Sequelize.DATE
-    }),
-     
-    'SubTransaction': db.define('SubTransaction', {
-      id: Sequelize.INTEGER,
-      amount: Sequelize.STRING,
-      from_id: Sequelize.INTEGER,
-      to_id: Sequelize.INTEGER
-    })
-  };
+    var Sequelize = require('sequelize');
 
-  // create all tables in database if they do not exist
-  db.sync();
+    var db = new Sequelize('socialmoneyjs', 'root', 'root', {
+        host: "localhost",
+        port: 3306
+    });
 
-  return models;
+    var models = {
+       'User': db.define('User', {
+        id: Sequelize.INTEGER,
+        username: Sequelize.STRING,
+        password: Sequelize.STRING,
+        email: Sequelize.STRING
+      }),
+       
+      'Transaction': db.define('Transaction', {
+        id: Sequelize.INTEGER,
+        title: Sequelize.STRING,
+        description: Sequelize.TEXT,
+        date: Sequelize.DATE
+      }),
+       
+      'SubTransaction': db.define('SubTransaction', {
+        id: Sequelize.INTEGER,
+        amount: Sequelize.STRING,
+        from_id: Sequelize.INTEGER,
+        to_id: Sequelize.INTEGER
+      })
+    };
+
+    // create all tables in database if they do not exist
+    db.sync();
+
+    return models;
+  }
 };
