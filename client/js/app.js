931ee66d1af5ace7a0e9e982336a49f6d0e7f9b9
Vanja Cosic <vanjacosic@gmail.com>
2012-08-24 22:36:38 +0200

    Added simple user details view

--- client/js/app.js
+++ client/js/app.js
@@ -30,8 +30,16 @@ var users = {};
                 listView = $(this.el);
                 
             $(this.el).empty();
-            users.each(function(activity){
-                listView.append(template(activity.toJSON()));
+            users.each(function(user){
+                var renderedItem = template(user.toJSON()),
+                    $renderedItem = $(renderedItem);  //convert the html into an jQuery object
+                    //console.log(user.get('id'));
+                    $renderedItem.jqmData('userId', user.get('id'));  //set the data on it for use in the click event
+                $renderedItem.bind('click', function(){
+                    //set the activity id on the page element for use in the details pagebeforeshow event
+                    $('#user-details').jqmData('userId', $(this).jqmData('userId'));  //'this' represents the element being clicked
+                });
+                listView.append($renderedItem);
             });
             container.html($(this.el));
             container.trigger('create');
@@ -44,6 +52,24 @@ var users = {};
                 
             usersList.append(template(item.toJSON()));
             usersList.listview('refresh');
+        }
+    });
+
+    users.UserDetailsView = Backbone.View.extend({
+        //since this template will render inside a div, we don't need to specify a tagname
+        initialize: function() {
+            this.template = _.template($('#user-details-template').html());
+        },
+        
+        render: function() {
+            //console.log(this.model);
+            var container = this.options.viewContainer,
+                user = this.model,
+                renderedContent = this.template(this.model.toJSON());
+                
+            container.html(renderedContent);
+            container.trigger('create');
+            return this;
         }
     });
     
@@ -64,8 +90,21 @@ $('#home').live('pageinit', function(event){
 
 $('#add-button').live('click', function(){
     
-    users.users.add({id: 6, name: 'Linus Torvalds', balance: '96'});
+    users.users.add({id: 6, firstName: 'Linus', lastName: 'Torvalds', balance: '96'});
 });
+
+
+$('#user-details').live('pagebeforeshow', function(){
+    console.log('userId: ' + $('#user-details').jqmData('userId'));
+    var usersDetailsContainer = $('#user-details').find(":jqmData(role='content')"),
+        userDetailsView,
+        userId = $('#user-details').jqmData('userId'),
+        usersModel = users.users.get(userId);
+
+    userDetailsView = new users.UserDetailsView({model: users.users.get(userId), viewContainer: usersDetailsContainer});
+    userDetailsView.render();
+});
+
 
 // login button
 $('#login-button').live('click', function(){
